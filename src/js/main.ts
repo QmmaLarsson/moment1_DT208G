2728f795ce8f2bd0a3c60ea5868ce9f995214caf
Emma Larsson <emma940112@gmail.com>
2024-03-30 01:48:00 +0100

    Har skapar funktioner för att spara information i localStorage och för att hämta information från localStorage

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,9 @@
+//Startfunktion som laddar in kurser sparade i local storage
+window.onload = init;
+function init() {
+    loadFromLocalStorage();
+}
+
 //Interface för kurs
 interface Course {
     code: string;
@@ -6,16 +12,53 @@ interface Course {
     url: string;
 }
 
+//Funktion för att spara kurser i localStorage
+function saveToLocalStorage(course: Course): void {
+    //Läs in befintlig information från localStorage
+    const storedCourses: string | null = localStorage.getItem("courses");
+
+    //Spara befintlig information i en array
+    let storedCoursesArr: Course[] = [];
+    if (storedCourses) {
+        storedCoursesArr = JSON.parse(storedCourses);
+    }
+
+    //Lägg till ny information till den befintliga
+    storedCoursesArr.push(course);
+
+    //Lagrar arrayen i en JSON-string och lagrar i localStorage
+    localStorage.setItem("courses", JSON.stringify(storedCoursesArr));
+}
+
+//Funktion för att visa sparade kurser när sidan läses in
+function loadFromLocalStorage(): void {
+    //Läs in befintlig information från localStorage
+    const storedCourses: string | null = localStorage.getItem("courses");
+
+    //Spara befintlig information i en array
+    let storedCoursesArr: Course[] = [];
+    if (storedCourses) {
+        storedCoursesArr = JSON.parse(storedCourses);
+    }
+
+    //Loopa igenom varje sparad kurs och skicka informationen till funktionen displayCourses
+    storedCoursesArr.forEach((course: Course) => {
+        displayCourses(course);
+    });
+}
+
 //Funktion för att skriva ut kurser
 function displayCourses(course: Course): void {
-    const savedCourses = document.getElementById("savedCourses");
-    if (savedCourses) {
-        savedCourses.innerHTML = `
+    const courseContainer = document.getElementById("courseContainer") as HTMLDivElement;
+    if (courseContainer) {
+        const courseElement = document.createElement("div") as HTMLDivElement;
+        courseElement.innerHTML = `
         <p><strong>Kurskod:</strong> ${course.code}</p>
         <p><strong>Kursnamn:</strong> ${course.name}</p>
         <p><strong>Progression:</strong> ${course.progression}</p>
         <p><strong>URL till kursplan:</strong> ${course.url}</p>
         `;
+        courseContainer.appendChild(courseElement);
     }
 }
 
@@ -39,6 +82,12 @@ courseForm.addEventListener("submit", (event) => {
         progression: courseProgression.value,
         url: courseUrl.value
     }
+
+    //Rensar formuläret
+    courseForm.reset();
+
+    //Sparar kursen i local storage
+    saveToLocalStorage(newCourse);
 
     //Använd display courses för att skriva ut kursinfo
     displayCourses(newCourse);
